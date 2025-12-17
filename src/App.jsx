6ff3f05b10d5cad4554866a9ac1892b29410a7cf
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-17 22:14:29 +0530

    key

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,61 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const[color, setColor] = useState(null);
+  const [theme, setTheme] = useState("light");
+
+
+  const themes = {
+    light: {
+      label: "Light",
+      bg: "bg-white",
+      text: "text-black",
+    },
+
+    red: {
+      label: "Red",
+      bg: "bg-red-500",
+      text: "text-white",
+    },
+
+    orange: {
+      label: "Orange",
+      bg: "bg-orange-500",
+      text: "text-white",
+    },
+
+    black: {
+      label: "Black",
+      bg: "bg-gray-900",
+      text: "text-white",
+    },
+
+    green: {
+      label: "Green",
+      bg: "bg-green-800",
+      text: "text-white",
+    },
+
+  };
 
   return (
     <>
-
       <div className="min-h-screen flex items-center justify-center">
         <div className="w-full max-w-xl min-h-[80px] p-4 border border-gray-300 rounded-lg shadow-sm flex items-center justify-center">
           <div>
             <h2 className="text-3xl font-bold text-center ">Multi Toggle App</h2>
             <div className='mt-4  flex justify-center gap-3 '  >
-              <button className='cursor-pointer px-3 border border-gray-300 rounded-sm transition-transform duration-100 hover:scale-110'><span>Red</span></button>
-              <button className='cursor-pointer px-3 border border-gray-300 rounded-sm transition-transform duration-100 hover:scale-110'   ><span>Green</span></button>
-              <button className='cursor-pointer px-3 border border-gray-300 rounded-sm  transition-transform duration-100 hover:scale-110 ' ><span>Black</span></button>
-              <button className='cursor-pointer  px-3 border border-gray-300 rounded-sm transition-transform duration-100 hover:scale-110 '  ><span>Orange</span></button>
+              {Object.keys(themes).map((key) =>
+                <button
+                  key={key}
+                  className='cursor-pointer px-3 py-1 border border-gray-300 rounded-sm
+                 transition-transform duration-100 hover:scale-110'>
+                  {themes[key].label}
+                </button>
+              )}
             </div>
           </div>
-
         </div>
       </div>
-
     </>
   )
 }
