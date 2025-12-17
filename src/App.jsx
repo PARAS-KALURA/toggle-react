e9c1774d2239650bba7744d24cc02138ca4638ee
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-17 18:29:51 +0530

    UI Done

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ const App = () => {
         <div className="w-full max-w-xl min-h-[80px] p-4 border border-gray-300 rounded-lg shadow-sm flex items-center justify-center">
           <div>
             <h2 className="text-3xl font-bold text-center ">Multi Toggle App</h2>
-            <div className='mt-4  '  >
-              <button className='cursor-pointer px-3 border-1 border-gray-300 rounded-sm mr-3'   ><span>Red</span></button>
-              <button className='cursor-pointer px-3 border-1 border-gray-300 rounded-sm mr-3 '   ><span>Green</span></button>
-              <button className='cursor-pointer px-3 border-1 border-gray-300 rounded-sm mr-3 ' ><span>Black</span></button>
-              <button className='cursor-pointer  px-3 border-1 border-gray-300 rounded-sm '  ><span>Orange</span></button>
+            <div className='mt-4  flex justify-center gap-3 '  >
+              <button className='cursor-pointer px-3 border border-gray-300 rounded-sm '   ><span>Red</span></button>
+              <button className='cursor-pointer px-3 border border-gray-300 rounded-sm '   ><span>Green</span></button>
+              <button className='cursor-pointer px-3 border border-gray-300 rounded-sm  ' ><span>Black</span></button>
+              <button className='cursor-pointer  px-3 border border-gray-300 rounded-sm '  ><span>Orange</span></button>
             </div>
           </div>
 
